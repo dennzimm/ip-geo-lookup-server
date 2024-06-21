a3ba8c6766550564b9e3a7f50976fb372901609a
dennzimm <dennzimm@icloud.com>
2024-06-21 18:17:42 +0200

    feat(geolocation): validate geolocation request params

--- src/api/geolocation/geolocation.routes.ts
+++ src/api/geolocation/geolocation.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import GeolocationController from "./geolocation.controller";
+import { validateRequestMiddleware } from "../../middlewares/validate-request.middleware";
+import { geolocationRequestSchema } from "./schemas/geolocation-request.schema";
 
 export const geolocationRouter: Router = (() => {
   const router = Router();
 
-  router.get("/:ip", GeolocationController.getGeolocationByIp);
+  router.get(
+    "/:ip",
+    validateRequestMiddleware(geolocationRequestSchema),
+    GeolocationController.getGeolocationByIp
+  );
 
   return router;
 })();
---
+++ src/api/geolocation/schemas/geolocation-request.schema.ts
@@ -0,0 +1,10 @@
+import z from "zod";
+
+export const geolocationRequestSchema = z.object({
+  params: z.object({
+    ip: z.string().ip("Invalid IP address"),
+  }),
+});
+
+export type GeolocationRequest = z.infer<typeof geolocationRequestSchema>;
+export type GeolocationRequestParams = GeolocationRequest["params"];
